360cd8c4db14f7a6b4344f671b81acdde4e4d207
Emmy Morris <emmymorris1102@gmail.com>
2021-06-08 09:40:27 -0600

    Set up db and relationships

---
+++ db/migrate/20210608153421_create_competition_teams.rb
@@ -0,0 +1,9 @@
+class CreateCompetitionTeams < ActiveRecord::Migration[5.2]
+  def change
+    create_table :competition_teams do |t|
+      t.references :competition, foreign_key: true
+      t.references :team, foreign_key: true
+      t.timestamps
+    end
+  end
+end
