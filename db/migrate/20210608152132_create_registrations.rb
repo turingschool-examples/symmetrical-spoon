c57f21d965d8a679e3af6f86d16b93f45e41e6e8
Aliya Merali <merali4@gmail.com>
2021-06-08 09:23:22 -0600

    Update ruby version, create and run migrations

---
+++ db/migrate/20210608152132_create_registrations.rb
@@ -0,0 +1,10 @@
+class CreateRegistrations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :registrations do |t|
+      t.references :team, foreign_key: true
+      t.references :competition, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
