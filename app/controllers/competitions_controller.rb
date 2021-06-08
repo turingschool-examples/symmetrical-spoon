68fe9f89a8c72d1f3e7041da9047b683ab77d4ff
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-08 11:08:39 -0600

    Implement actions to submit a new team

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -5,7 +5,22 @@ class CompetitionsController < ApplicationController
 
   def show
     @competition = Competition.find(params[:id])
-    # binding.pry
     @players_avg_age = @competition.average_age.round(1)
+  end
+
+  def new
+    @competition = Competition.find(params[:id])
+  end
+
+  def create
+    @competition = Competition.find(params[:id])
+    @competition.teams << Team.create(team_params)
+    redirect_to "/competitions/#{@competition.id}"
+  end
+
+  private
+
+  def team_params
+    params.permit(:hometown, :nickname)
   end
 end
