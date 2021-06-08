7523afef91005aeed024199852fadd8cd7e5d6f4
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-08 09:55:07 -0600

    Create and pass tests for user story 1

---
+++ app/controllers/competitions_controller.rb
@@ -0,0 +1,10 @@
+class CompetitionsController < ApplicationController
+
+  def index
+    @competitions = Competition.all
+  end
+
+  def show
+  end
+
+end
---
+++ spec/features/competition/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Competitions Index' do
+  before :each do
+    @competition_1 = Competition.create!(name: 'Extreme Pogs', location: 'Thunder Dome', sport: 'Pogs')
+    @competition_2 = Competition.create!(name: 'Snakes and Danger', location: 'Mount Everest', sport: 'Snakes and Ladders')
+    @competition_3 = Competition.create!(name: 'Jack be Nimble', location: 'Mount Vesuveous', sport: 'Jacks')
+  end
+  describe 'visit' do
+    it 'has the names of all competitions' do
+      visit '/competitions'
+
+      expect(page).to have_content('Extreme Pogs')
+      expect(page).to have_content('Snakes and Danger')
+      expect(page).to have_content('Jack be Nimble')
+    end
+
+    it 'has links to all competitions' do
+      visit '/competitions'
+
+      expect(page).to have_link('Extreme Pogs')
+      expect(page).to have_link('Snakes and Danger')
+      expect(page).to have_link('Jack be Nimble')
+
+      click_link('Extreme Pogs')
+
+      expect(page).to have_current_path("/competitions/#{@competition_1.id}")
+    end
+  end
+end
