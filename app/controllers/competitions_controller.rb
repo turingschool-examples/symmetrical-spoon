e4dfbde9b48faa2d06f79f077f5c3fbc4569474a
Suzie K <suziekim.dev@gmail.com>
2021-06-08 11:49:22 -0400

    Add show action

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -3,4 +3,8 @@ class CompetitionsController < ApplicationController
   def index
     @competitions = Competition.all
   end
+
+  def show 
+    @competition = Competition.find(params[:id])
+  end
 end
