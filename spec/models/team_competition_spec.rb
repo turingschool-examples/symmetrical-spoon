5aa9788b2238c5beef3e1b38cc36b5274f1552f6
deebot10 <dee1jhill@gmail.com>
2021-06-08 10:13:19 -0600

    Set up complete

--- spec/models/team_competition_spec.rb
+++ spec/models/team_competition_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Player do
+RSpec.describe TeamCompetition do
   describe 'relationships' do
     it {should belong_to :team}  
+    it {should belong_to :competition} 
   end     
 end
