2a0a70b410403135cf88439204405dd5c74a4e4a
Richard <rdesilvey@gmail.com>
2021-06-08 10:08:42 -0600

    Add show spec for a competition

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,3 +2,8 @@ class CompetitionsController < ApplicationController
   def index
     @competitions = Competition.all
   end
+
+  def show
+
+  end
+end
--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "The show page for a competition" do
-  
+RSpec.describe 'The show page for a competition,' do
+
+  describe 'competition details,' do
+    it 'shows the competition name'
+    it 'shows the competition location'
+    it 'shows the competition sport'
+  end
+
+  describe 'team list,' do
+    it 'shows the name and hometown of all the teams for this competition'
+    it 'shows the average age of all players in competition'
+  end
 end
