e09224e9eab13fc2dc38dc4753bd0624381e9e30
Caleb Wittman <calebjwittman1991@gmail.com>
2021-06-08 11:10:53 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get '/teams', to: 'teams#index'
 
   get '/competitions', to: 'competitions#index'
-  get '/competitions/:id/new', to: 'team_competitions#new'
   get '/competitions/:id', to: 'competitions#show'
-  post '/competitions/:id', to: 'team_competitions#create'
+
+  get '/team_competitions/:id/new', to: 'team_competitions#new'
+  post '/team_competitions/:id', to: 'team_competitions#create'
 end
