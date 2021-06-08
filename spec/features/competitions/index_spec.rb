a33b41d74e41ee74367b1546f9e2c272a3f580b1
Suzie K <suziekim.dev@gmail.com>
2021-06-08 11:38:33 -0400

    Red test shows names of all competitions

--- spec/features/competitions/index_spec.rb
+++ spec/features/competitions/index_spec.rb
@@ -10,7 +10,17 @@ RSpec.describe 'competition index page' do
   describe 'display' do
     before :each do 
       indigo = Competition.create!(name: "Indigo League", location: "Louisville", sport:"rocket launching")
-      scarlet = Competition.create!(name: "Scarlet League", location: "Dencer", sport: "tap dancing")
+      scarlet = Competition.create!(name: "Scarlet League", location: "Denver", sport: "tap dancing")
+      burgundy = Competition.create!(name: "Burgundy League", location: "Austin", sport: "extreme grilling")
     end
+
+    it 'shows names of all competitions' do
+      visit "/competitions"
+
+      expect(page).to have_content("Indigo League")
+      expect(page).to have_content("Scarlet League")
+    end
+
+    it 'links to each competition show page'
   end
 end
