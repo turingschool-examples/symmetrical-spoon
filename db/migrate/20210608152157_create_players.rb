494f200822a3735280b88ef18b90b9dd437b3079
Richard <rdesilvey@gmail.com>
2021-06-08 09:52:22 -0600

    make foreign key

--- db/migrate/20210608152157_create_players.rb
+++ db/migrate/20210608152157_create_players.rb
@@ -3,7 +3,7 @@ class CreatePlayers < ActiveRecord::Migration[5.2]
     create_table :players do |t|
       t.string :name
       t.integer :age
-      t.references :team
+      t.references :team, foreign_key: true
 
       t.timestamps
     end
