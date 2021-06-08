c13f57099094d27508a4b5cbbf5eb65a9198d45c
Leigh <lcpulzone@gmail.com>
2021-06-08 08:48:23 -0700

    Add joins table relationship

--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Competition, type: :model do
-  it { should have_many(:teams) }
+  it { should have_many(:games) }
+  it { should have_many(:teams).through(:games) }
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  it { should have_many(:competitions) }
+  it { should have_many(:games) }
+  it { should have_many(:competitions).through(:games) }
 end
