39fe2fe1fba09aa6dfa71a371d199eb8becea922
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-08 11:33:03 -0600

    Add error handling for non-existent teams

--- app/controllers/competition_entries_controller.rb
+++ app/controllers/competition_entries_controller.rb
@@ -6,12 +6,12 @@ class CompetitionEntriesController < ApplicationController
   def create
     competition = Competition.find(params[:competition_id])
     team = Team.find_by(nickname: params[:nickname], hometown: params[:hometown])
-    entry = CompetitionEntry.new(competition_id: competition.id, team_id: team.id)
-    if entry.save
+    if team.present?
+      entry = CompetitionEntry.create(competition_id: competition.id, team_id: team.id)
       redirect_to competition_path(competition.id)
     else
       redirect_to new_competition_competition_entry_path(competition.id)
-      # flash[:alert]
+      flash[:alert] = 'Error: Information entered does not match any team in our system'
     end
   end
 end
