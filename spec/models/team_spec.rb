e7a3fdedb9348d7199b03cde1198b8c7062dbcda
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-06-08 09:32:55 -0600

    Create models & model tests for relationships

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Team do
+  describe 'relationships' do
+    it {should have_many :players}
+    it {should have_many(:competition_teams)}
+    it {should have_many(:competitions).through(:competition_teams)}
+  end
+end
