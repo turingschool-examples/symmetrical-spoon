678634731c065bb03fdf594d2cc089d6630b8b45
Suzie K <suziekim.dev@gmail.com>
2021-06-08 12:32:48 -0400

    Green test average of all players in competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,4 +4,7 @@ class Competition < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :teams, through: :registrations 
 
+  def average_all_players_age
+    Team.joins(:registrations, :players).where("registrations.competition_id = ?", self.id).average("players.age").to_f
+  end
 end
