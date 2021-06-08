6364c0e92e88ddad234bd4d2b4fed7a9002613a9
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-08 10:28:03 -0600

    Implement test for user story 2; add class method to Player model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
+  has_many :players
   has_many :teams_in_plays
   has_many :competitions, through: :teams_in_plays
 
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Player do
+  before :each do
+    @team_1 = Team.create!(hometown: 'Burgess, PA', nickname: 'The Giants')
+    @team_2 = Team.create!(hometown: 'San Diego, CA', nickname: 'Salamanders')
+    @player_1 = @team_1.players.create!(name: "Henry", age: 13)
+    @player_2 = @team_1.players.create!(name: "Bob", age: 11)
+    @player_3 = @team_2.players.create!(name: "Gilfoyle", age: 16)
+    @player_4 = @team_2.players.create!(name: "Damian", age: 22)
+  end
 
   describe 'relationships' do
     it {should belong_to :team}
   end
+
+  describe 'class methods' do
+    it '::average_age' do
+      exp_avg = ((@player_1.age + @player_2.age +
+                 @player_3.age + @player_4.age) / 4.0)
+      expect(Player.average_age).to eq exp_avg
+    end
+  end
+
 end
