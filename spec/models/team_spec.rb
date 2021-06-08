98741f5c971bfa32e7312e53507ead2c02ca1012
Zach T <ztrokey@gmail.com>
2021-06-08 10:59:33 -0600

    add average_age to team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Team, type: :model do
     it {should have_many :competition_teams}
     it {should have_many(:competitions).through(:competition_teams)}
   end
+  describe '.average_age' do
+    it 'averages the age of the players on the team' do
+      comp1 = Competition.create!(name: 'Chuck Stuff', location: 'Denver', sport: 'Axe Throwing')
+      team1 = comp1.teams.create!(hometown: 'Denver', nickname: 'Team1')
+      player1 = team1.players.create!(name: 'P1', age: 20)
+      player3 = team1.players.create!(name: 'P3', age: 30)
+
+      expect(team1.average_age).to eq(25)
+    end
+  end
 end
