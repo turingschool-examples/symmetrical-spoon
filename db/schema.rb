d79c5133abf6f5217ec923c34ad5a132f7133f12
Zach T <ztrokey@gmail.com>
2021-06-08 09:58:04 -0600

    complete db setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_153522) do
+ActiveRecord::Schema.define(version: 2021_06_08_155135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_06_08_153522) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "team_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema.define(version: 2021_06_08_153522) do
 
   add_foreign_key "competition_teams", "competitions"
   add_foreign_key "competition_teams", "teams"
+  add_foreign_key "players", "teams"
 end
