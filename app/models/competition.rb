bd5598651fc9b8b909b6a54092729b65912789c5
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-08 11:01:34 -0600

    Add user story 2 with hacky avg_age method

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,4 +2,13 @@ class Competition < ApplicationRecord
   has_many :teams_in_plays
   has_many :teams, through: :teams_in_plays
 
+  def avg_age
+    # Wasted too much time messinmg around with joins query
+    # and had to implement a Ruby solution
+    total_age = 0
+    sum = self.teams.each do |team|
+      total_age += team.players.average_age
+    end
+    total_age / self.teams.length
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Competition do
+  before :each do
+    @finals = Competition.create!(name: 'NBA Finals', location: 'Orlando', sport: 'Basketball')
+    @world_cup = Competition.create!(name: 'The World Cup 2021', location: 'Berlin', sport: 'Soccer')
+    @team_1 = Team.create!(hometown: 'Burgess, PA', nickname: 'The Giants')
+    @team_2 = Team.create!(hometown: 'San Diego, CA', nickname: 'Salamanders')
+    @finals.teams << @team_1
+    @finals.teams << @team_2
+    @player_1 = @team_1.players.create!(name: "Henry", age: 13)
+    @player_2 = @team_1.players.create!(name: "Bob", age: 11)
+    @player_3 = @team_2.players.create!(name: "Gilfoyle", age: 16)
+    @player_4 = @team_2.players.create!(name: "Damian", age: 22)
+  end
 
   describe 'relationships' do
     it {should have_many :teams_in_plays}
     it {should have_many(:teams).through(:teams_in_plays)}
+  end
+
+  describe 'instance methods' do
+    it 'returns average age of competitors' do
+      exp_avg = ((@player_1.age + @player_2.age +
+                 @player_3.age + @player_4.age) / 4.0)
+      expect(@finals.avg_age).to eq exp_avg
+    end
   end
 end
