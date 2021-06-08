09aad11cb90af97caf044a6d0053e8a396e74221
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-06-08 09:53:13 -0600

    Initial commit

---
+++ spec/models/competition_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Competition, type: :model do
+  describe "relationships" do
+    it {should have_many :team_competitions}
+    it {should have_many(:teams).through(:team_competitions)}
+  end
+end
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  describe "relationships" do
+    it {should have_many  :players}
+    it {should have_many  :team_competitions}
+    it {should have_many(:competitions).through(:team_competitions)}
+  end
+end
