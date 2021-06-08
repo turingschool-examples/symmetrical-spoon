c4f076fdc6f68f72c64008e3755e5c9133f2a32c
Richard <rdesilvey@gmail.com>
2021-06-08 09:44:09 -0600

    Add spec files for competitions

---
+++ spec/features/competitions/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "The index page for competitions" do
+  before :each do
+    @comp1 = FactoryBot.create(:competition)
+    @comp2 = FactoryBot.create(:competition)
+    @comp3 = FactoryBot.create(:competition)
+
+    visit competitions_path
+  end
+
+  describe 'list of competitions,' do
+    it 'shows all the competitions with their name as a link' do
+      within '#competitions-list' do
+        expect(page).to have_link(@comp1.name, href: competition_path(@comp1))
+        expect(page).to have_link(@comp2.name, href: competition_path(@comp2))
+        expect(page).to have_link(@comp3.name, href: competition_path(@comp4))
+      end
+    end
+
+    it 'the name link navigates to the show page for that competition' do
+      within '#competitions-list' do
+        click_link @comp1.name
+      end
+      expect(current_path).to eq competition_path(@comp1)
+    end
+  end
+end
---
+++ spec/features/competitions/show_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "The show page for a competition" do
+  
+end
