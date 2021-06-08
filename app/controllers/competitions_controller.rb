aed00e4246f9a24c98ad74235dbb51dc382f5766
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-08 18:40:45 +0200

    Creates competition test and show page

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -3,4 +3,9 @@ class CompetitionsController < ApplicationController
   def index
     @competitions = Competition.all
   end
+
+  def show
+    @competition = Competition.find(params[:id])
+    @teams = Team.all
+  end
 end
