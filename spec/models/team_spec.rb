e081000b355fbb5355e5753abdda6c50672b2616
Zach T <ztrokey@gmail.com>
2021-06-08 10:44:18 -0600

    add players relationship to team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   describe 'relationships' do
+    it {should have_many :players}
     it {should have_many :competition_teams}
     it {should have_many(:competitions).through(:competition_teams)}
   end
